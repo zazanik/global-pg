95696893bcd35455d6998d6a19cdb8afc4073d70
fromthesecond <fromthesecond@gmail.com>
2016-03-18 18:42:52 +0200

    18.03.2016 did 1 page

---
+++ 11-servises-details.php
@@ -0,0 +1,57 @@
+<?php include "template/header.php"; ?>
+<a class="up-line" href="javascript:history.back();">
+    <div class="container">
+        <div class="text">Назад</div>
+    </div>
+</a>
+
+<div class="partners-details servises-details">
+    <?php include "template/baner-top.php"?>
+    <div class="container">
+        <div class="just-cont clearfix">
+            <h2>мероприятия Gpg</h2>
+            <div class="info-cont clearfix">
+                <img src="images/servises-img.png" alt=""/>
+                <p>стоимость: <span>по запросу</span></p>
+                <a class="button" id="order-service" href="#">заказать услугу</a>
+            </div>
+            <div class="text-cont clearfix">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+                    massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
+                    quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+                    massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
+                    quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.  Donec
+                    pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a,
+                    venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
+                    Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+                </p>
+            </div>
+        </div>
+
+        <div class="servises-photo-cont clearfix">
+            <a class="fancybox" rel="gallery3" href="images/dom_1.png" title="photo 1">
+                <img src="images/dom_1.png" alt="" />
+            </a>
+            <a class="fancybox" rel="gallery3" href="images/dom_1.png" title="photo 1">
+                <img src="images/dom_1.png" alt="" />
+            </a>
+            <a class="fancybox" rel="gallery3" href="images/dom_1.png" title="photo 1">
+                <img src="images/dom_1.png" alt="" />
+            </a>
+            <a class="fancybox" rel="gallery3" href="images/dom_1.png" title="photo 1">
+                <img src="images/dom_1.png" alt="" />
+            </a>
+        </div>
+        <a class="link" href="#">Назад</a>
+    </div>
+
+
+</div>
+
+
+
+<?php include "template/baner-bottom.php"; ?>
+<?php include "template/footer.php"; ?>
+
